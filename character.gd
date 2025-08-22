89a4a0d57e6dee55d2c457d7796400c3fbf2b8d6
Euang <euang@qq.com>
2025-08-23 00:14:01 +0800

    character finish

--- character.gd
+++ character.gd
@@ -1,16 +1,15 @@
+class_name Player
 extends CharacterBody2D
-
+const BULLET = preload("res://Bullet.tscn")
 enum State{
 	IDLE,
 	RUN,
 	RISE,
 	FALL,
-	ATK,
+	ATTACK,
 	HURT,
-	DIE,
 }
 enum Direction{LEFT=-1,RIGHT=1}
-
 
 var state_:State=State.IDLE
 var state_time_:float=0
@@ -19,16 +18,21 @@ var direction_:Direction=Direction.RIGHT:
 		direction_=v
 		if not is_node_ready():await ready
 		graphic.scale.x=direction_
+var hurtNum_:int=0
+var f_:Vector2=Vector2.ZERO
 @export var input_w:StringName
 @export var input_s:StringName
 @export var input_a:StringName
 @export var input_d:StringName
+@export var input_j:StringName
 @export var speedRun:float=100
 
 @onready var graphic: Node2D = $Graphic
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var timer: Timer = $Timer
 
 func _physics_process(delta: float) -> void:
+	var isOnFloor=is_on_floor()
 	var inputX=Input.get_axis(input_a,input_d)
 	#1.状态判断
 	var nextState:State=state_
@@ -38,15 +42,37 @@ func _physics_process(delta: float) -> void:
 			else:nextState=State.RUN
 			if velocity.y>0:nextState=State.FALL
 			if velocity.y<0:nextState=State.RISE
+			if Input.is_action_just_pressed(input_j):nextState=State.ATTACK
 		State.RUN:
 			if is_zero_approx(inputX):nextState=State.IDLE
 			if velocity.y>0:nextState=State.FALL
 			if velocity.y<0:nextState=State.RISE
+			if Input.is_action_just_pressed(input_j):nextState=State.ATTACK
 		State.RISE:
 			if velocity.y>0:nextState=State.FALL
+			if Input.is_action_just_pressed(input_j):nextState=State.ATTACK
 		State.FALL:
 			if velocity.y<0:nextState=State.RISE
 			if is_on_floor():nextState=State.IDLE
+			if Input.is_action_just_pressed(input_j):nextState=State.ATTACK
+		State.ATTACK:
+			if animation_player.is_playing():
+				#if Input.is_action_just_pressed(input_j):
+					#animation_player.seek(0)
+				pass
+			else:
+				if is_on_floor():
+					nextState=State.IDLE
+				else:
+					if velocity.y>0:nextState=State.FALL
+					elif velocity.y<0:nextState=State.RISE
+		State.HURT:
+			if timer.is_stopped():
+				if velocity.y:nextState=State.FALL
+				else :nextState=State.IDLE
+	if hurtNum_>0:
+		nextState=State.HURT
+		hurtNum_-=1
 	#2.状态切换
 	if nextState==state_:pass
 	else:
@@ -59,29 +85,40 @@ func _physics_process(delta: float) -> void:
 				animation_player.play("rise")
 			State.FALL:
 				animation_player.play("fall")
+			State.ATTACK:
+				animation_player.play("attack")
+				var b=BULLET.instantiate()
+				b.global_position=global_position-Vector2(0,25)
+				b.from=self
+				b.velocity=Vector2.RIGHT*direction_*500
+				b.scale.x=direction_
+				Global.add_child(b)
+			State.HURT:pass
 		state_=nextState
 		state_time_=0
 	#3.状态执行
-	match state_:
-		State.IDLE:
-			velocity.x=0
-			if Input.is_action_just_pressed(input_w):velocity.y=-400
-		State.RUN:
-			velocity.x=inputX*speedRun
-			if Input.is_action_just_pressed(input_w):velocity.y=-400
-		State.RISE:
-			velocity.x=inputX*speedRun
-		State.FALL:
-			velocity.x=inputX*speedRun
-	#绝对执行
-	state_time_+=delta
-	if not is_zero_approx(inputX):
-		direction_=Direction.LEFT if inputX<0 else Direction.RIGHT
+	if state_==State.HURT:
+		velocity+=f_*delta
+	else:
+		match state_:
+			State.IDLE:
+				velocity.x=0
+				if Input.is_action_just_pressed(input_w):velocity.y=-400
+			State.RUN:
+				velocity.x=inputX*speedRun
+				if Input.is_action_just_pressed(input_w):velocity.y=-400
+			State.RISE:
+				velocity.x=inputX*speedRun
+			State.FALL:
+				velocity.x=inputX*speedRun
+			State.ATTACK:
+				if isOnFloor&&Input.is_action_just_pressed(input_w):velocity.y=-400
+				velocity.x=inputX*speedRun
+		if not is_zero_approx(inputX):
+			direction_=Direction.LEFT if inputX<0 else Direction.RIGHT
+		
 	if Input.is_action_just_pressed(input_s):set_collision_mask_value(1,0)
 	if Input.is_action_just_released(input_s):set_collision_mask_value(1,1)
-	move(delta)
-
-
-func move(delta:float)->void:
+	state_time_+=delta
 	velocity.y+=1000*delta
 	move_and_slide()
